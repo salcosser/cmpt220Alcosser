33bfbc89fcf2d15fda6781e90a33b8925a39e899
Sam Alcosser <samalcosser@gmail.com>
2019-01-24 12:05:38 -0500

    Completed Hello World assignment

---
+++ prj/0/driver_proj0.java
@@ -0,0 +1,7 @@
+//This simple program was made by Sam Alcosser
+public class driver_proj0 {
+	public static void main(String[] args) {
+		//this prints hello world
+		System.out.println("Hello World!");
+	}
+}
